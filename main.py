34ecbbe86d374bf873b38fbc7867eb8976047f94
whatsagqoogol <cristylepgarrard@protonmail.com>
2020-09-20 07:23:14 +0000

    starting to connect to data api

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import requests
+import pandas as pd
 
 app = Flask(__name__)
 
 # most recent year in this database is 2018
 url = 'https://educationdata.urban.org/api/v1/schools/ccd/directory/2018/'
+response = requests.get(url)
+
+df = pd.DataFrame(response.json())
+print(df.head())
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/report_case')
+def report_case(data=df):
+  data = data.jsonify()
+  return data
+  # return render_template('report_case.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080, debug=True)
